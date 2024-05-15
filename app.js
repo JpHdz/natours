82ecc9095ed51d8478be34f2c4cfbeeafbf96e44
Juan Pablo Hernandez <jpnator18@gmail.com>
2024-05-15 12:39:59 -0600

    Added a comment

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const reviewRouter = require('./routes/reviewRoute');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
